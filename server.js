7e08693ed93257f21b901f72084f8f0807d6d15b
AmbikaSakaray <ambikasakaray4@gmail.com>
2026-06-15 13:38:32 +0530

    added files

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -9,7 +11,12 @@ const productRoutes =require("./routes/productRoutes");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+
+// --- UPDATE THESE TWO LINES TO ALLOW LARGER PAYLOADS ---
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// -----------------------------------------------------
+
 app.use(contactRoutes);
 app.use(productRoutes);
 
@@ -27,7 +34,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, () => {
   console.log(`Server Running On Port ${PORT}`);
